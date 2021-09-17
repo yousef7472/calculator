46f2bf6a923cf6f5c7e379e303e3e275fe126ee6
yousef7472 <yousef7shaban@gmail.com>
2021-09-17 20:11:38 +0300

    create

---
+++ js/js.js
@@ -0,0 +1,66 @@
+
+//            all buttons
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        
+        switch(e.target.innerText){
+           case 'C':
+                display.innerText = '';
+            break;
+
+            case '←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+                }
+            break;
+            
+            case '×':
+                 display.innerText += '*'
+            break;
+
+            case '÷':
+                 display.innerText += '/'
+            break;
+                 
+            case '=':
+                
+             try{
+
+                display.innerText = eval(display.innerText);
+
+                if (eval(display.innerText) == undefined) {
+                    display.innerText = 'ايرور بطعم الليمون'
+                    }
+                
+             }
+
+             catch {
+                display.innerText = 'ايرور سادة';
+                
+            }
+            break;
+            default:
+                 display.innerText += e.target.innerText
+            }
+             
+});
+    
+});
+
+ 
+//            for dark mode
+var icon = document.getElementById("icon");
+        icon.onclick = function() {
+            document.body.classList.toggle("dark-theme");
+            if(document.body.classList.contains("dark-theme")) {
+            icon.src = "imgs/223-2232869_png-file-svg-moon-icon-circle-png-transparent-removebg-preview.png";
+        }
+        else {
+            icon.src = "imgs/sunny.png";
+        }
+};
